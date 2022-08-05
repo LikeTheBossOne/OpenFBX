0f05cd1959f3343a0339212f0a6053d944941371
Brendan Boss <blboss@ncsu.edu>
2022-08-04 21:16:38 -0500

    Updating gitignore

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "OpenFBX"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.c",
+		"src/**.cpp"
+	}
+
+	filter "system:linux"
+		pic "On"
+		
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
